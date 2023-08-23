00ff90aa16fa2fe3c3dba8dce6101a7073f7efb1
rai-n <neeraj.rai@city.ac.uk>
2023-08-23 01:59:32 +0100

    Added resume

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
 
     useEffect(() => {
 
-
+        document.title = "Neeraj Rai | Portfolio"
         const handleScroll = () => {
 
             if (window.scrollY > 200) {
@@ -76,6 +76,7 @@ const Navbar: React.FC = () => {
 
                 if (res.statusCode == 200) {
                     localStorage.setItem('downloaded_resume', 'true')
+                    console.log('downloaded clicked, incremented')
                 }
 
             } catch (err: any) {
@@ -142,16 +143,16 @@ const Navbar: React.FC = () => {
             </div>
             <div className="flex flex-col mr-10">
                 <div>
-                    {/* <a href="/path-to-your-resume.pdf" download> */}
-                    <button onClick={handleClick} className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-600 focus:outline-none">
-                        Download Resume
-                        <svg className="h-5 w-5 ml-2 inline-block" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
-                            <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="#1C274C" stroke-width="1.5" />
-                        </svg>
-                    </button>
-                    {/* </a> */}
+                    <a href="/resume/Resume.pdf" download>
+                        <button onClick={handleClick} className="bg-red-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-red-600 focus:outline-none">
+                            Download Resume
+                            <svg className="h-5 w-5 ml-2 inline-block" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 7L12 14M12 14L15 11M12 14L9 11" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M16 17H12H8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" />
+                                <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="#1C274C" stroke-width="1.5" />
+                            </svg>
+                        </button>
+                    </a>
 
                 </div>
                 <div>
